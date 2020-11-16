4208959884c0cfca52ed11607b9840063282ac27
Aaron Martin <aaron@eri.ucsb.edu>
2020-11-16 13:47:51 -0800

    Update to Credits.php

--- About/Credits.php
+++ About/Credits.php
@@ -6,22 +6,22 @@
     $hd->htmlBeg();
 ?>
 <section>
-    <h2>Development Team</h2>
+    <h2>Development Team:</h2>
     <ul>
-        <li>Aaron Martin - Initial Creation and Development Lead</li>
+        <li>Aaron Martin - Initial Creation and Development Lead (2020-??-??)</li>
     </ul>
 </section>
 <section>
-    <h2>Technologies</h2>
-    <p>Want to keep the site relatively simple and self-contained</p>
+    <h2>Technologies:</h2>
+    <p>Striving to keep the site relatively simple and self-contained</p>
     <ul>
         <li>PHP-7.3+</li>
         <li>Javascript</li>
         <li>CSS</li>
+        <li>SQLite-3.x</li>
     </ul>
 </section>
 
 <?php
-
     $hd->htmlEnd();
 ?>
